e58b24d413e00441e8f72d598e7ef19ea89dd269
ifoxxiedev <obede.drrt.info@gmail.com>
2024-09-30 10:13:14 -0300

    chore: setup tailwind and poppins font

--- devcustomers/electron.vite.config.ts
+++ devcustomers/electron.vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig, externalizeDepsPlugin } from 'electron-vite'
 import react from '@vitejs/plugin-react'
+import tailwindCss from 'tailwindcss'
 
 export default defineConfig({
   main: {
@@ -10,6 +11,20 @@ export default defineConfig({
     plugins: [externalizeDepsPlugin()]
   },
   renderer: {
+    define: {
+      // Define the process.platform inside renderer process layer
+      'process.platform': JSON.stringify(process.platform)
+    },
+    // Add the tailwindcss plugin to the postcss configuration
+    css: {
+      postcss: {
+        plugins: [
+          tailwindCss({
+            config: './src/renderer/tailwind.config.js'
+          })
+        ]
+      }
+    },
     resolve: {
       alias: {
         '@renderer': resolve('src/renderer/src')
--- devcustomers/src/renderer/src/App.tsx
+++ devcustomers/src/renderer/src/App.tsx
@@ -1,12 +1,7 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
-
 function App(): JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
   return (
     <>
-      <h1>Hello</h1>
+      <h1 className="text-red-600 font-bold text-7xl">Hello</h1>
     </>
   )
 }
